fefdf62875241d1ae97e932803a137448b26a22f
Rodja Trappe <rodja@hoccer.com>
2011-03-23 15:53:53 +0100

    added locationmamager

--- SimpleMessageDemo/src/com/hoccer/demos/simplemessage/SimpleMessageDemo.java
+++ SimpleMessageDemo/src/com/hoccer/demos/simplemessage/SimpleMessageDemo.java
@@ -3,11 +3,35 @@ package com.hoccer.demos.simplemessage;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.hoccer.api.ClientConfig;
+import com.hoccer.api.android.AsyncLinccer;
+import com.hoccer.api.android.LinccLocationManager;
+
 public class SimpleMessageDemo extends Activity {
-    /** Called when the activity is first created. */
+    private LinccLocationManager mLocationManager;
+    private AsyncLinccer         mLinccer;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ClientConfig config = new ClientConfig("SimpleMessageDemo/Android",
+                "e101e890ea97012d6b6f00163e001ab0", "JofbFD6w6xtNYdaDgp4KOXf/k/s=");
+
+        mLinccer = new AsyncLinccer(config);
+        mLocationManager = new LinccLocationManager(this, mLinccer);
+    }
+
+    @Override
+    protected void onResume() {
+        mLocationManager.activate();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mLocationManager.deactivate();
+        super.onPause();
     }
 }
