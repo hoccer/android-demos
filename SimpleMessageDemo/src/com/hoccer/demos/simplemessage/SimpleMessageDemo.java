85bd1a126de6051c52c4da2af79c89d5d1e55de6
Rodja Trappe <rodja@hoccer.com>
2011-03-23 17:14:20 +0100

    added send button

--- SimpleMessageDemo/src/com/hoccer/demos/simplemessage/SimpleMessageDemo.java
+++ SimpleMessageDemo/src/com/hoccer/demos/simplemessage/SimpleMessageDemo.java
@@ -1,7 +1,16 @@
 package com.hoccer.demos.simplemessage;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -9,6 +18,7 @@ import com.hoccer.api.ClientConfig;
 import com.hoccer.api.EnvironmentStatus;
 import com.hoccer.api.android.AsyncLinccer;
 import com.hoccer.api.android.LinccLocationManager;
+import com.hoccer.api.android.AsyncLinccer.MessageType;
 
 public class SimpleMessageDemo extends Activity {
     private LinccLocationManager mLocationManager;
@@ -25,12 +35,20 @@ public class SimpleMessageDemo extends Activity {
 
         mLinccer = new AsyncLinccer(config);
         mLocationManager = new LinccLocationManager(this, mLinccer);
+
+        Button sendButton = (Button) findViewById(R.id.send);
+        sendButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                sendMessage(((EditText) findViewById(R.id.new_message)).getText().toString());
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         startLocationUpdates();
-
         super.onResume();
     }
 
@@ -40,7 +58,34 @@ public class SimpleMessageDemo extends Activity {
         super.onPause();
     }
 
+    private void sendMessage(String text) {
+        try {
+            mLinccer.asyncShare("one-to-many", new JSONObject("{message : '" + text + "'}"),
+                    new Handler() {
+                        @Override
+                        public void handleMessage(Message msg) {
+                            super.handleMessage(msg);
+                            if (msg.what != MessageType.SHARED) {
+                                runOnUiThread(new Runnable() {
+
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(SimpleMessageDemo.this, "sending failed",
+                                                Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }
+                        }
+                    });
+        } catch (JSONException e) {
+            Toast.makeText(SimpleMessageDemo.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
     private void startLocationUpdates() {
+        updateStatus();
+        mLocationManager.activate();
+
         mUpdateThread = new Thread() {
             @Override
             public void run() {
@@ -66,9 +111,6 @@ public class SimpleMessageDemo extends Activity {
     }
 
     private void updateStatus() {
-        updateStatus();
-        mLocationManager.activate();
-
         runOnUiThread(new Runnable() {
 
             @Override
