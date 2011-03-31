ad31f952c84190a6b1bd9d0687d8dae9be93fc9c
Mamta Bansal <mamta@hoccer.com>
2011-03-31 13:46:44 +0200

    showing image

--- PictureSlider/src/com/hoccer/pictureslider/PictureSlider.java
+++ PictureSlider/src/com/hoccer/pictureslider/PictureSlider.java
@@ -2,6 +2,7 @@ package com.hoccer.pictureslider;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class PictureSlider extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,8 @@ public class PictureSlider extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ImageView image = (ImageView) findViewById(R.id.image);
+        image.setImageResource(R.drawable.architecture);
     }
 }
