c9d25696e18c0eedbbd0ed0ba2ee154b21a2361c
Mamta Bansal <mamta@hoccer.com>
2011-03-31 14:01:11 +0200

    showing different pics on tab

--- PictureSlider/src/com/hoccer/pictureslider/PictureSlider.java
+++ PictureSlider/src/com/hoccer/pictureslider/PictureSlider.java
@@ -2,16 +2,46 @@ package com.hoccer.pictureslider;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
 public class PictureSlider extends Activity {
-    /** Called when the activity is first created. */
+    protected static final String TAG               = "PictureSlider";
+    private int                   currentImageIndex = 0;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        ImageView image = (ImageView) findViewById(R.id.image);
-        image.setImageResource(R.drawable.architecture);
+        displayNextImage();
+
+        final ImageView image = (ImageView) findViewById(R.id.image);
+        image.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                displayNextImage();
+            }
+        });
+
+    }
+
+    private void displayNextImage() {
+        final ImageView image = (ImageView) findViewById(R.id.image);
+
+        if (currentImageIndex == 0) {
+            image.setImageResource(R.drawable.architecture);
+        } else if (currentImageIndex == 1) {
+            image.setImageResource(R.drawable.grouping);
+        } else if (currentImageIndex == 2) {
+            image.setImageResource(R.drawable.api);
+            currentImageIndex = 0;
+        } else {
+            currentImageIndex = 0;
+        }
+
+        currentImageIndex++;
     }
 }
